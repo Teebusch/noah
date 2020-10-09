353bd0bec59fe1ae74cd89e3d055b886a37eba97
Tobias Busch <teebusch@gmail.com>
2020-10-09 10:58:25 +0200

    create Ark class and fpseudonymize function
    prototype

--- R/ark.R
+++ R/ark.R
@@ -1,16 +1,91 @@
-#' Title
+#' A Class to represent an ark.
 #'
-#' Does some cool stuff
-#' more later
+#' An Ark object can create unique pseudonyms.
+#' Given the same input, it will always return the same psuedonym.
+#' No pseudonym will repeat.
+Ark <- R6::R6Class("Ark",
+  public = list(
+    #' @description
+    #' Create new arc object.
+    #' @return A new `Ark` object.
+    initialize = function() {
+      private$pseudonym_parts <- list(
+        c("Alert", "Brazen", "Clever", "Docile", "Eager"),
+        c("Ant", "Bear", "Cat", "Dog", "Eagle", "Friendly")
+      )
+      private$max_length <- prod(lengths(private$pseudonym_parts))
+      self$log <- hash::hash()
+    },
+
+    #' @description
+    #' Print Ark object.
+    #' @examples
+    #' ark <- Ark()
+    #' print(ark)
+    print = function() {
+      cat("Ark of size ", private$max_length, "\n", sep = "")
+      if (self$length() > 0) {
+        log_entries <- paste(hash::keys(private$log), '→',
+                             hash::values(private$log))
+        cat("Logbook:\n--------\n")
+        cat(log_entries, sep = "\n")
+      } else {
+        cat("no one on board")
+      }
+      invisible(self)
+    },
+
+    length = function(...) {
+      length(self$log)
+    },
+
+    as.list = function() {
+      list(1,2,3)
+    },
+
+    pseudonymize = function(...) {
+      dots <- list(...)
+      uid <- digest::digest(dots)
+      pseudonym <- "foo bar"
+      self$log[[uid]] <- pseudonym
+    },
+
+    log = NULL
+  ),
+  private = list(
+    pseudonym_parts = NULL,
+    max_length = NULL
+  )
+)
+
+
+#' Length of an Ark
 #'
-#' @param x description
-#' @param y description
+#' @param x
+#' @param ...
 #'
-#' @return numeric
+#' @return
 #' @export
 #'
 #' @examples
-ark <- function(x, y) {
-  "Hello noah!"
-  x + y
+length.Ark <- function(obj, ...) {
+  obj$length()
+}
+
+#' Create unique pseudonyms
+#'
+#' This is a pipe-friendly function.
+#'
+#' @param ... One or multiple objects for which pseudonyms should be created,
+#' usually one or more columns of a data frame. All objects must be of the same
+#' length.
+#' @param .ark An Ark object. By default a new Ark is created. Using an existing
+#' Ark makes sure that the same input returns the same pseudonym.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pseudonymize <- function(..., .ark = Ark$new()){
+  .ark$pseudonymize(...)
 }
--- tests/testthat/test-ark.R
+++ tests/testthat/test-ark.R
@@ -1,3 +1,26 @@
-test_that("ark works", {
-  expect_equal(ark(1, 1), 2)
+test_that("Ark object can be created", {
+  ark <- Ark$new()
+  expect_s3_class(ark, "Ark")
+  expect_s3_class(ark, "R6")
+})
+
+test_that("A single value can be pseudonymized", {
+  res <- pseudonymize("Mata Hari")
+  expect_true(res != "Mata Hari")
+  expect_type(res, "character")
+})
+
+test_that("Ark length method works", {
+  ark <- Ark$new()
+  expect_equal(ark$length(), 0)
+  expect_equal(length(ark), 0)
+
+  ark$pseudonymize("Mata Hari")
+  expect_equal(ark$length(), 1)
+  expect_equal(length(ark), 1)
+})
+
+test_that("Multiple values can be pseudonymized", {
+  res <- pseudonymize(c("Mata Hari", "James Bond", "Lewis Carroll"))
+  expect_equal(length(res), 3)
 })
