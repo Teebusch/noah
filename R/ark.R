74dbc0bc074349339e08809c21a9bab16aae0679
Tobias Busch <teebusch@gmail.com>
2020-10-09 20:09:10 +0200

    combine pseudonyms from parts of words

--- R/ark.R
+++ R/ark.R
@@ -1,80 +1,99 @@
 #' A Class to represent an ark.
 #'
+#' @description
 #' An Ark object can create unique pseudonyms.
 #' Given the same input, it will always return the same psuedonym.
 #' No pseudonym will repeat.
+
 Ark <- R6::R6Class("Ark",
   public = list(
-    #' @description
-    #' Create new arc object.
+
+    #' @field log Hashtable for all used pseudonyms. Inputs (keys) are stored
+    #' as hashes.
+    log = NULL,
+
+    #' @description Create new arc object.
     #' @return A new `Ark` object.
     initialize = function() {
-      private$pseudonym_parts <- list(
-        c("Alert", "Brazen", "Clever", "Docile", "Eager"),
-        c("Ant", "Bear", "Cat", "Dog", "Eagle", "Friendly")
-      )
-      private$max_length <- prod(lengths(private$pseudonym_parts))
+      private$max_length <- prod(lengths(private$parts))
       self$log <- hash::hash()
     },
 
-    #' @description
-    #' Print Ark object.
-    #' @examples
-    #' ark <- Ark()
-    #' print(ark)
+    #' @description Create Pseudonyms for input.
+    #' @param ... One or more R objects.
+    #' @return Character vector of pseudonyms with same length as input.
+    pseudonymize = function(...) {
+      dots <- list(...)
+      uid <- digest::digest(dots)
+
+      if (hash::has.key(uid, self$log)) {
+        return(self$log[[uid]])
+      } else {
+        index <- self$length() + 1
+        self$log[uid] <- private$index_to_pseudonym(index)
+      }
+    },
+
+    #' @description Pretty-print an Ark object.
     print = function() {
-      cat("Ark of size ", private$max_length, "\n", sep = "")
+      cat("Ark with maximum size ", private$max_length, "\n", sep = "")
       if (self$length() > 0) {
-        log_entries <- paste(hash::keys(private$log), '→',
-                             hash::values(private$log))
+        log_entries <- paste(
+          hash::keys(private$log), ">>",
+          hash::values(private$log)
+        )
         cat("Logbook:\n--------\n")
         cat(log_entries, sep = "\n")
       } else {
-        cat("no one on board")
+        cat("The Ark is empty. Use `Ark$pseudomymize(...) to add entries.`")
       }
       invisible(self)
     },
 
+    #' @description Number of used pseudonyms in an Ark.
+    #' @param ... Other arguments passed on to `length()`
     length = function(...) {
-      length(self$log)
-    },
-
-    as.list = function() {
-      list(1,2,3)
-    },
-
-    pseudonymize = function(...) {
-      dots <- list(...)
-      uid <- digest::digest(dots)
-      pseudonym <- "foo bar"
-      self$log[[uid]] <- pseudonym
-    },
-
-    log = NULL
+      length(self$log, ...)
+    }
   ),
+
   private = list(
-    pseudonym_parts = NULL,
-    max_length = NULL
+
+    #' @field parts Words that will be combined to form pseudonyms.
+    parts = list(
+      c("Alert", "Brazen", "Clever", "Docile", "Eager"),
+      c("Ant", "Bear", "Cat", "Dog", "Eagle", "Fox")
+    ),
+
+    #' @field max_length Maximum number of possible pseudonyms in the Ark.
+    max_length = NULL,
+
+    #' @description Returns the pseudonym corresponding to an index.
+    #' @param index An integer or a vector of integers between 1 and the Ark's
+    #' max_length.
+    #' @return A character vector of pseudomyms with the same length as the
+    #' input
+    index_to_pseudonym = function(index) {
+      assertthat::assert_that(
+        all(dplyr::between(index, 1, private$max_length))
+      )
+      k <- index - 1
+      n <- lengths(private$parts)[2]
+      i <- (k %/% n) + 1
+      j <- (k %% n) + 1
+      paste(private$parts[[1]][i], private$parts[[2]][j])
+    }
   )
 )
 
 
-#' Length of an Ark
-#'
-#' @param x
-#' @param ...
-#'
-#' @return
 #' @export
-#'
-#' @examples
-length.Ark <- function(obj, ...) {
-  obj$length()
-}
+length.Ark <- function(obj, ...) obj$length()
 
-#' Create unique pseudonyms
+
+#' Create unique pseudonyms.
 #'
-#' This is a pipe-friendly function.
+#' Pseudonymize returns unique pseudonyms for R objects
 #'
 #' @param ... One or multiple objects for which pseudonyms should be created,
 #' usually one or more columns of a data frame. All objects must be of the same
@@ -82,10 +101,11 @@ length.Ark <- function(obj, ...) {
 #' @param .ark An Ark object. By default a new Ark is created. Using an existing
 #' Ark makes sure that the same input returns the same pseudonym.
 #'
-#' @return
+#' @return A character vector of pseudonyms.
 #' @export
 #'
 #' @examples
-pseudonymize <- function(..., .ark = Ark$new()){
+#' pseudonymize("Mata Hari")
+pseudonymize <- function(..., .ark = Ark$new()) {
   .ark$pseudonymize(...)
 }
--- tests/testthat/test-ark.R
+++ tests/testthat/test-ark.R
@@ -20,7 +20,26 @@ test_that("Ark length method works", {
   expect_equal(length(ark), 1)
 })
 
+test_that("Creating pseudonyms from index works", {
+  ark <- Ark$new()
+  max_length <- ark$.__enclos_env__$private$max_length
+  f <- ark$.__enclos_env__$private$index_to_pseudonym  # shortcut
+  expect_equal(f(1), "Alert Ant")
+  expect_equal(f(c(1,2,3)), c("Alert Ant", "Alert Bear", "Alert Cat"))
+  expect_length(f(1:max_length), max_length)
+  expect_type(f(1:max_length), "character")
+})
+
+test_that("Fail when requesting more pseudonyms than available", {
+  ark <- Ark$new()
+  max_length <- ark$.__enclos_env__$private$max_length
+  f <- ark$.__enclos_env__$private$index_to_pseudonym  # shortcut
+  expect_error(f(max_length+1))
+  expect_error(f(1:(max_length+1)))
+})
+
 test_that("Multiple values can be pseudonymized", {
   res <- pseudonymize(c("Mata Hari", "James Bond", "Lewis Carroll"))
   expect_equal(length(res), 3)
 })
+
