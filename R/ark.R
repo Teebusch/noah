95b86469cf51e6c6e885c4237186e7247909a233
Tobias Busch <teebusch@gmail.com>
2020-10-23 00:06:19 +0200

    implement add_pseudonyms
    also added pipe operator

--- R/ark.R
+++ R/ark.R
@@ -113,6 +113,42 @@ length.Ark <- function(x) x$length()
 #'
 #' @examples
 #' pseudonymize("Mata Hari")
-pseudonymize <- function(..., .ark = Ark$new()) {
+pseudonymize <- function(..., .ark = NULL) {
+  if (is.null(.ark)) {
+    .ark <- Ark$new()
+  } else {
+    assertthat::assert_that(inherits(.ark, "Ark"))
+  }
   .ark$pseudonymize(...)
+}
+
+
+#' Add column with pseudonyms to a data frame
+#'
+#' @param .data A data frame to add pseudonyms to.
+#' @param ... Columns on which pseudonyms should be based. Supports tidy select.
+#' If empty, all columns will be used.
+#' @param .name Name of the new column as string.
+#' @param .before,.after Destination of new column, passed on to
+#' `dplyr::relocate()`
+#' @param .ark Object of class `Ark`. If NULL (default) a new `Ark` is
+#' created.
+#'
+#' @return A data frame with an additional column containing the pseudonyms.
+#' @export
+#'
+#' @examples
+#' add_pseudonyms(mtcars)
+add_pseudonyms <- function(.data, ..., .name = "pseudonym", .before = NULL,
+                           .after = NULL, .ark = NULL) {
+
+  if (...length() > 0) {
+    pn <- pseudonymize(dplyr::select(.data, ...), .ark = .ark)
+  } else {
+    pn <- pseudonymize(.data, .ark = .ark)
+  }
+
+  .data %>%
+    dplyr::mutate(!!.name := pn) %>%
+    dplyr::relocate(!!.name, .before = !!.before, .after = !!.after)
 }
