457cb5cf854c8fdb58959e3b46ca29340c076b33
Tobias Busch <teebusch@gmail.com>
2020-10-24 13:41:54 +0200

    improve ark print function

--- R/ark.R
+++ R/ark.R
@@ -43,18 +43,40 @@ Ark <- R6::R6Class("Ark",
     },
 
     #' @description Pretty-print an Ark object.
-    print = function() {
-      cat("Ark with maximum size ", private$max_length, "\n", sep = "")
-      if (self$length() > 0) {
-        log_entries <- paste(
-          hash::keys(self$log), ">>",
-          hash::values(self$log)
-        )
-        cat("Logbook:\n--------\n")
-        cat(log_entries, sep = "\n")
+    print = function(n = NULL) {
+
+      # summary
+      perc_full <- self$length() / private$max_length
+      cat(sprintf(
+        "# An Ark: %i / %i (%0.0f%%)\n",
+        self$length(), private$max_length, perc_full
+      ))
+
+      # entries
+      if (self$length() == 0) {
+        cat("The Ark is empty.")
+      } else if (self$length() >= private$max_length) {
+        cat("The Ark is full")
       } else {
-        cat("The Ark is empty. Use `Ark$pseudomymize(...) to add entries.`")
+        if (is.null(n)) {
+          n <- 10
+        } else {
+          assertthat::assert_that(is.numeric(n))
+          assertthat::assert_that(n > 0)
+        }
+        n_max <- length(self$log)
+        i_max <- min(n, n_max)
+        i     <- 1:i_max
+        k     <- hash::keys(self$log)[1:i_max]
+        v     <- hash::values(self$log)[1:i_max]
+
+        cat(sprintf("%*s [key] %*s [pseudonym]\n", nchar(i_max), " ", 5, " "))
+        cat(sprintf("%*s %.8s... %s", nchar(i_max), i, k, v), sep = "\n")
+        if (i_max < n_max) {
+          cat(sprintf("# ...with %i more entries", n_max - i_max))
+        }
       }
+
       invisible(self)
     },
 
